a63864304a79a1d2736abe5106609a93b7c8f2b0
damir <damirexzael@gmail.com>
2017-10-12 22:38:22 -0300

    Create CellNSSet

---
+++ plugins/optim/src/strategy/ibex_CellNSSet.h
@@ -0,0 +1,71 @@
+//============================================================================
+//                                  I B E X
+// File        : ibex_CellNSSet.h
+// Author      : Matias Campusano, Damir Aliquintui, Ignacio Araya
+// Copyright   : Ecole des Mines de Nantes (France)
+// License     : See the LICENSE file
+// Created     : Oct 12, 2017
+// Last Update : Oct 12, 2017
+//============================================================================
+
+#ifndef __IBEX_CELL_NS_SET_H__
+#define __IBEX_CELL_NS_SET_H__
+
+#include "ibex_CellBuffer.h"
+#include <set>
+#include <list>
+
+namespace ibex {
+
+	struct maxsize {
+	  bool operator() (const Cell* c1, const Cell* c2) const
+	  {
+		  int n = c1->box.size();
+
+		  float areaC1 = c1->box[n-1].diam()*c1->box[n-2].diam();
+		  float areaC2 = c2->box[n-1].diam()*c2->box[n-2].diam();
+		  if(areaC1 != areaC2) return (areaC1 > areaC2);
+		  if(c1->get<CellBS>().depth != c2->get<CellBS>().depth) return (c1->get<CellBS>().depth < c2->get<CellBS>().depth);
+		  return (c1->get<CellBS>().id > c2->get<CellBS>().id);
+	  }
+	};
+
+	class CellNSSet: public CellBufferOptim {
+		public:
+			CellNSSet();
+
+			virtual ~CellNSSet();
+
+			  void flush();
+
+			  /** Return the size of the buffer. */
+			  unsigned int size() const;
+
+			  /** Return true if the buffer is empty. */
+			  bool empty() const;
+
+			  /** push a new cell on the stack. */
+			  void push(Cell* cell);
+
+			  /** Pop a cell from the stack and return it.*/
+			  Cell* pop();
+
+			  /** Return the next box (but does not pop it).*/
+			  Cell* top() const;
+
+
+			  virtual double minimum() const;
+
+			  virtual void contract(double loup);
+
+		private:
+			/* Set of Cells */
+			  typename std::set<Cell*, maxsize> nondset;
+			/* Stack of cells */
+			  typename std::list<Cell*> dset;
+	};
+
+
+} // namespace ibex
+
+#endif /* __IBEX_CELL_NS_SET_H__ */
