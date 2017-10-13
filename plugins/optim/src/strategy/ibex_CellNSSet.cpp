3708e70064fa936b381ec8317567b5ec2039f253
damir <damir@iaraya-server.inf.ucv.cl>
2017-10-13 18:59:36 -0300

    Fixed

--- plugins/optim/src/strategy/ibex_CellNSSet.cpp
+++ plugins/optim/src/strategy/ibex_CellNSSet.cpp
@@ -48,7 +48,8 @@ namespace ibex {
 		bool dominated = false;
 		int n;
 		std::multiset<Cell*, maxsize>::iterator it;
-		for (it = nondset.begin(); it != nondset.end(); ++it) {
+		std::multiset<Cell*, maxsize>::iterator aux;
+		for (it = nondset.begin(); it != nondset.end(); ) {
 			n = (*it)->box.size();
 			// si cell es dominado por un no dominado
 			if((*it)->box[n-1].lb() < cell->box[n-1].lb() && (*it)->box[n-2].lb() < cell->box[n-2].lb()) {
@@ -58,8 +59,11 @@ namespace ibex {
 			// si cell domina a un no dominado
 			if(cell->box[n-1].lb() < (*it)->box[n-1].lb() && cell->box[n-2].lb() < (*it)->box[n-2].lb()) {
 				dset.push_front(*it);
+				aux = it;
+				++aux;
 				nondset.erase(*it);
-			}
+				it=aux;
+			} else ++it;
 		}
 		if(dominated) dset.push_front(cell);
 		else nondset.insert(cell);
@@ -71,31 +75,40 @@ namespace ibex {
 		typename std::list<Cell*> domcells;
 		std::list<Cell*>::iterator it;
 		std::list<Cell*>::iterator it1;
+		std::list<Cell*>::iterator aux;
 		std::multiset<Cell*, maxsize>::iterator it2;
+		std::multiset<Cell*, maxsize>::iterator aux2;
 		Cell* c = *nondset.begin();
+		std::cout << "NS pop " << c  << std::endl;
 		nondset.erase(nondset.begin());
 		// se agregan todos los cell dominados por c en dset a la lista domcells
-		for (it = dset.begin(); it != dset.end(); ++it) {
+		for (it = dset.begin(); it != dset.end();) {
 			n = (*it)->box.size();
 			if(c->box[n-1].lb() < (*it)->box[n-1].lb() && c->box[n-2].lb() < (*it)->box[n-2].lb()) {
 				domcells.push_front(*it);
+				aux = it;
+				++aux;
 				dset.erase(it);
-			}
+				it=aux;
+			} else ++it;
 		}
 		// se cambian todos los cell que esten dominados en domcells a la lista dset
-		for (it = domcells.begin(); it != domcells.end(); ++it) {
+		for (it = domcells.begin(); it != domcells.end(); ) {
 			n = (*it)->box.size();
 			for (it1 = domcells.begin(); it1 != domcells.end(); ++it1) {
 				n1 = (*it1)->box.size();
 				if((*it1)->box[n1-1].lb() < (*it)->box[n-1].lb() && (*it1)->box[n1-2].lb() < (*it)->box[n-2].lb()) {
 					dset.push_front(*it);
+					aux = it;
+					++aux;
 					domcells.erase(it);
-				}
+					it=aux;
+				} else ++it;
 			}
 		}
 		// se agrega a nondom si un elemento de domcells no es dominado por ningun elemento de nonset
 		for (it = domcells.begin(); it != domcells.end(); ++it) {
-			for (it2 = nondset.begin(); it2 != nondset.end(); ++it2) {
+			for (it2 = nondset.begin(); it2 != nondset.end(); ) {
 				// si es dominado el elemento de domcells
 				if((*it2)->box[n-1].lb() < (*it)->box[n-1].lb() && (*it2)->box[n-2].lb() < (*it)->box[n-2].lb()) {
 					dominated = true;
@@ -104,18 +117,22 @@ namespace ibex {
 				// si domina el elemento de domcells a uno de nondset
 				if((*it)->box[n-1].lb() < (*it2)->box[n-1].lb() && (*it)->box[n-2].lb() < (*it2)->box[n-2].lb()) {
 					dset.push_front(*it2);
+					aux2 = it2;
+					++aux2;
 					nondset.erase(it2);
-				}
+					it2=aux2;
+				}else ++it2;
 			}
 			if(dominated) dset.push_back(*it);
 			else nondset.insert(*it);
-			domcells.erase(it);
+
+
 		}
 		return c;
 	}
 
 	Cell* CellNSSet::top() const{
-		return *dset.begin();
+		return *nondset.begin();
 	}
 
 	double CellNSSet::minimum() const{
