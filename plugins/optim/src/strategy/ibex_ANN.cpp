6517260998394c7498609869d327a2e95f7429af
damir <damir@iaraya-server.inf.ucv.cl>
2018-01-31 12:53:51 -0300

    cleaning ANN

--- plugins/optim/src/strategy/ibex_ANN.cpp
+++ plugins/optim/src/strategy/ibex_ANN.cpp
@@ -82,52 +82,6 @@ vector<double> ANN::testingNeuron(vector<double> &inputVals, vector<double> &tar
 	//showVectorVals("Targets:", targetVals);
 	assert(targetVals.size() == topology.back());
 
-	// Report how well the training is working, average over recnet
-	// cout << "Net recent average error: "
-	//	 << myNet->getRecentAverageError() << endl;
-
-	/*
-	int auxTarget=0, auxResults=0;
-	// any contract result
-	if(targetVals[targetVals.size()-1] == 1) {
-		for(int i=0; i< targetVals.size();i++) {
-			if(resultVals[i] > 0.5) auxResults++;
-		}
-		if(resultVals[targetVals.size()-1] > 0.5 || auxResults > 0) VAC++;
-		else FAC++;
-	} else {
-		for(int i=0; i< targetVals.size();i++) {
-			if(targetVals[i] > 0.5) auxTarget++;
-		}
-		// no contract
-		if(auxTarget == 0) {
-			for(int i=0; i< resultVals.size();i++) {
-				if(resultVals[i] > 0.7) auxResults++;
-			}
-			if(auxResults == 0) VNC++;
-			else FNC++;
-		// contract
-		} else {
-			int i = 0;
-			for(i=0; i< resultVals.size();i++) {
-				if(resultVals[i] < 0.5 && targetVals[i] == 1) break;
-			}
-			if(i == resultVals.size()) VC++;
-			else FC++;
-
-		}
-	}
-
-	cout << "VNC " << VNC << " FNC " << FNC << endl;
-	cout << "VC " << VC << " FC " << FC << endl;
-	cout << "VAC " << VAC << " FAC " << FAC << endl;
-	float verdadero, falso;
-	verdadero = (float) (VNC+VC+VAC)/(VNC+VC+VAC+FNC+FC+FAC);
-	falso = (float) (FNC+FC+FAC)/(VNC+VC+VAC+FNC+FC+FAC);
-	cout << "verdadero " << verdadero << " falso " << falso << endl;
-	// if(resultVals[targetVals.size()-1] > 0.5 || auxResults > 0) getchar();
-	 */
-
 	return resultVals;
 }
 
@@ -142,73 +96,6 @@ ANN::ANN(int inputSize) : trainingPass(0), totalTraining(10) {
 
 	// crea la red neuronal con la topologia obtenida
 	myNet = new Network(topology);
-
-	/*
-	vector<double> inputVals, targetVals, resultVals;
-
-	while(!trainData.isEof())
-		{
-			++trainingPass;
-			cout << endl << "Pass " << trainingPass;
-
-			// Get new input data and feed it forward:
-			if(trainData.getNextInputs(inputVals) != topology[0])
-				break;
-			showVectorVals(": Inputs :", inputVals);
-			myNet->feedForward(inputVals);
-
-			// Collect the net's actual results:
-			myNet->getResults(resultVals);
-			showVectorVals("Outputs:", resultVals);
-
-			// Train the net what the outputs should have been:
-			trainData.getTargetOutputs(targetVals);
-			showVectorVals("Targets:", targetVals);
-			assert(targetVals.size() == topology.back());
-
-			myNet->backProp(targetVals);
-
-			// Report how well the training is working, average over recnet
-			cout << "Net recent average error: "
-			     << myNet->getRecentAverageError() << endl;
-
-			if(trainingPass > totalTraining) break;
-		}
-
-		// get error ANN
-		cout << "Net recent average error: "
-	                     << myNet->getRecentAverageError() << endl;
-
-
-		cout << "testing data" << endl;
-
-		while(!trainData.isEof())
-		{
-			++trainingPass;
-			cout << endl << "Pass " << trainingPass;
-
-			// Get new input data and feed it forward:
-			if(trainData.getNextInputs(inputVals) != topology[0])
-				break;
-			showVectorVals(": Inputs :", inputVals);
-			myNet->feedForward(inputVals);
-
-			// Collect the net's actual results:
-			myNet->getResults(resultVals);
-			showVectorVals("Outputs:", resultVals);
-
-			// Train the net what the outputs should have been:
-			trainData.getTargetOutputs(targetVals);
-			showVectorVals("Targets:", targetVals);
-			assert(targetVals.size() == topology.back());
-
-			// Report how well the training is working, average over recnet
-			cout << "Net recent average error: "
-				 << myNet->getRecentAverageError() << endl;
-
-			if(trainingPass > totalTraining + 5) break;
-		}
-		*/
 
 }
 
