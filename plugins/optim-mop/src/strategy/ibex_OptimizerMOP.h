1f577242dc62e4a950f47fb5480b8483e1316b0e
Ignacio <rilianx@gmail.com>
2019-06-12 23:53:38 +0000

    fixing zoom out

--- plugins/optim-mop/src/strategy/ibex_OptimizerMOP.h
+++ plugins/optim-mop/src/strategy/ibex_OptimizerMOP.h
@@ -312,6 +312,10 @@ public:
 				myfile >> y2_lb >> y2_ub;
 				focus[0] = Interval(y1_lb,y1_ub);
 				focus[1] = Interval(y2_lb,y2_ub);
+				if(instruction == "zoom_out")
+				{		focus2[0]=BxpMOPData::y1_init;
+						focus2[1]=BxpMOPData::y2_init;}
+
 				cout << instruction << focus << endl;
 				if(instruction=="zoom_out" || instruction=="zoom_in"){
 					for(auto cc:paused_cells){
