54677b693bbcd9152908f63948c45bb6c724d870
Alen Figueroa <alen.figueroa.m@mail.pucv.cl>
2019-09-19 15:20:26 -0300

    Agregada clase TestOptimizerMOP, agregado makefile para test unitarios

---
+++ plugins/optim-mop/tests/TestOptimizerMOP.cpp
@@ -0,0 +1,53 @@
+/* ============================================================================
+ * I B E X - Optimizer Tests
+ * ============================================================================
+ * Copyright   : IMT Atlantique (FRANCE)
+ * License     : This program can be distributed under the terms of the GNU LGPL.
+ *               See the file COPYING.LESSER.
+ *
+ * Author(s)   : Gilles Chabert
+ * Created     : Mar 2, 2012
+ * Last update : Oct 17, 2017
+ * ---------------------------------------------------------------------------- */
+
+#include "TestOptimizerMOP.h"
+
+
+using namespace std;
+
+namespace ibex {
+
+void TestOptimizerMOP::vec_problem01() {
+
+	
+	CPPUNIT_ASSERT(true);
+}
+
+void TestOptimizerMOP::vec_problem02() {
+	
+	CPPUNIT_ASSERT(true);
+}
+
+
+
+void TestOptimizerMOP::issue50_1() {
+	CPPUNIT_ASSERT(true);
+}
+
+void TestOptimizerMOP::issue50_2() {
+	CPPUNIT_ASSERT(true);
+}
+
+void TestOptimizerMOP::issue50_3() {
+	CPPUNIT_ASSERT(true);
+}
+
+void TestOptimizerMOP::issue50_4() {
+	CPPUNIT_ASSERT(true);
+}
+
+void TestOptimizerMOP::unconstrained() {
+	CPPUNIT_ASSERT(true);
+}
+
+} // end namespace
