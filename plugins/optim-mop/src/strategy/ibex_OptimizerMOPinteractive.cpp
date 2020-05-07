66ddceeaa426314ca75e2d1961a2cdc420141c12
Ignacio <rilianx@gmail.com>
2020-05-07 21:37:21 +0000

    ordering functions

--- plugins/optim-mop/src/strategy/ibex_OptimizerMOPinteractive.cpp
+++ plugins/optim-mop/src/strategy/ibex_OptimizerMOPinteractive.cpp
@@ -40,6 +40,7 @@ void OptimizerMOP_I::write_envelope(string output_file){
 }
 
 
+
 IntervalVector OptimizerMOP_I::load(const IntervalVector& init_box, string filename) {
 
 	nb_cells=0;
@@ -92,6 +93,112 @@ IntervalVector OptimizerMOP_I::load(const IntervalVector& init_box) {
 	//return _optimize(init_box, focus);
 }
 
+void OptimizerMOP_I::save_state_in_file(string filename){
+	 //write object into the file
+	 // Object to write in file
+	 ofstream file_obj;
+
+	 // Opening file in append mode
+	 file_obj.open(filename, ios::out);
+	 file_obj.write((char*) &BxpMOPData::y1_init, sizeof(BxpMOPData::y1_init));
+	 file_obj.write((char*) &BxpMOPData::y2_init, sizeof(BxpMOPData::y2_init));
+
+	 int len = cells.size();
+	 file_obj.write((char*) &len, sizeof(int));
+	 for (auto c:cells){
+		 file_obj.write((char*) &c->box[0], sizeof(c->box[0])*c->box.size());
+		 file_obj.write((char*) &cdata->a, sizeof(cdata->a));
+		 file_obj.write((char*) &cdata->w_lb, sizeof(cdata->w_lb));
+		 file_obj.write((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
+	 }
+
+	 len = paused_cells.size();
+	 file_obj.write((char*) &len, sizeof(int));
+	 for (auto c:paused_cells){
+		 file_obj.write((char*) &c->box[0], sizeof(c->box[0])*c->box.size());
+		 file_obj.write((char*) &cdata->a, sizeof(cdata->a));
+		 file_obj.write((char*) &cdata->w_lb, sizeof(cdata->w_lb));
+		 file_obj.write((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
+	 }
+
+	 len = ndsH.size();
+
+	 file_obj.write((char*) &len, sizeof(int));
+	 for (auto elem:ndsH.NDS2){
+		 file_obj.write((char*) &elem.first[0], sizeof(elem.first[0])*elem.first.size());
+		 file_obj.write((char*) &elem.second.n, sizeof(elem.second.n));
+		 if(elem.second.n >= 1) file_obj.write((char*) &elem.second.x1[0], sizeof(elem.second.x1[0])*elem.second.x1.size());
+		 if(elem.second.n == 2) file_obj.write((char*) &elem.second.x2[0], sizeof(elem.second.x2[0])*elem.second.x2.size());
+	 }
+
+	 for (auto p : ndsH.NDS2) cout << p.first << endl;
+
+	 file_obj.close();
+}
+
+void OptimizerMOP_I::load_state_from_file(string filename, const IntervalVector& init_box){
+	// Object to read from file
+	ifstream file_obj;
+
+	// Opening file in input mode
+	file_obj.open(filename, ios::in);
+	file_obj.read((char*)&BxpMOPData::y1_init, sizeof(BxpMOPData::y1_init));
+	file_obj.read((char*)&BxpMOPData::y2_init, sizeof(BxpMOPData::y2_init));
+
+	int len;
+	file_obj.read((char*) &len, sizeof(int));
+	for (int i=0; i<len; i++){
+		Cell* c=new Cell(init_box);
+		file_obj.read((char*)&c->box[0], sizeof(c->box[0])*c->box.size());
+		c->prop.add(new BxpMOPData());
+		buffer.add_property(c->box, c->prop);
+		bsc.add_property(c->box, c->prop);
+		ctc.add_property(c->box, c->prop);
+
+		file_obj.read((char*) &cdata->a, sizeof(cdata->a));
+		file_obj.read((char*) &cdata->w_lb, sizeof(cdata->w_lb));
+		file_obj.read((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
+		cells.insert(c);
+	}
+
+	file_obj.read((char*) &len, sizeof(int));
+	for (int i=0; i<len; i++){
+		Cell* c=new Cell(init_box);
+		file_obj.read((char*)&c->box[0], sizeof(c->box[0])*c->box.size());
+		c->prop.add(new BxpMOPData());
+		buffer.add_property(c->box, c->prop);
+		bsc.add_property(c->box, c->prop);
+		ctc.add_property(c->box, c->prop);
+
+		file_obj.read((char*) c->prop[BxpMOPData::id], sizeof(BxpMOPData));
+		cells.insert(c);
+	}
+
+	file_obj.read((char*) &len, sizeof(int));
+	ndsH.NDS2.clear ();
+
+	for (int i=0; i<len; i++){
+		Vector y(2);
+		int nn;
+
+		file_obj.read((char*) &y[0], sizeof(y[0])*2);
+		file_obj.read((char*) &nn, sizeof(int));
+		Vector x1(1);
+		Vector x2(1); //or n+2?
+
+		if(nn>=1) {x1.resize(n); file_obj.read((char*) &x1[0], sizeof(x1[0])*(n));}
+		if(nn==2) {x2.resize(n); file_obj.read((char*) &x2[0], sizeof(x2[0])*(n));}
+
+		ndsH.NDS2[y]=NDS_data(x1,x2);
+		ndsH.NDS2[y].n=nn;
+	}
+	cout.precision(17);
+	for (auto p : ndsH.NDS2) cout << p.first << endl;
+
+
+	file_obj.close();
+}
+
 void OptimizerMOP_I::update_refpoint(Vector& refpoint){
 	this->refpoint = refpoint;
   //we move from paused_cells to cells, all the boxes with lb dominated by refpoint
@@ -125,6 +232,7 @@ OptimizerMOP_I::IStatus OptimizerMOP_I::run(int maxiter, double eps) {
 		buffer.pop();
 		cells.erase(c);
 
+
     //we verify that the box_lb dominates the ref_point, otherwise it is paused
 		IntervalVector boxy = get_boxy(c->box,n);
 		if(refpoint[0] < boxy[0].lb() || refpoint[1] < boxy[1].lb() ){
@@ -150,6 +258,9 @@ OptimizerMOP_I::IStatus OptimizerMOP_I::run(int maxiter, double eps) {
 		contract_and_bound(*c, initbox);
 		if (c->box.is_empty()) { delete c; continue; }
 
+		//Aquí se debería imprimir (para visualización):
+		//c->id, c->parent->id, c->dist
+
     //upper_bounding
 		upper_bounding(c->box);
 
@@ -162,7 +273,7 @@ OptimizerMOP_I::IStatus OptimizerMOP_I::run(int maxiter, double eps) {
 		pair<Cell*,Cell*> new_cells;
 		try {
 			new_cells=pair<Cell*,Cell*>(bsc.bisect(*c));
-			delete c; // deletes the cell.
+			delete c; // deletes the cell. Se mantiene par visualización
 		}
 		catch (NoBisectableVariableException& ) {
 			throw NoBisectableVariableException();
@@ -173,6 +284,9 @@ OptimizerMOP_I::IStatus OptimizerMOP_I::run(int maxiter, double eps) {
 
 		buffer.push(new_cells.second);
 		cells.insert(new_cells.second);
+
+
+
     /************* end of bisection *************/
 
 	}
--- plugins/optim-mop/src/strategy/ibex_OptimizerMOPinteractive.h
+++ plugins/optim-mop/src/strategy/ibex_OptimizerMOPinteractive.h
@@ -42,118 +42,20 @@ public:
   */
 
   typedef enum {NONE, READY, STOPPED, FINISHED} IStatus;
-  IStatus run(int maxiter=5, double eps=1e-1);
 
-  void save_state_in_file(string filename){
-     //write object into the file
-     // Object to write in file
-     ofstream file_obj;
+  /*Funciones para interactuar con la api */
 
-     // Opening file in append mode
-     file_obj.open(filename, ios::out);
-     file_obj.write((char*) &BxpMOPData::y1_init, sizeof(BxpMOPData::y1_init));
-     file_obj.write((char*) &BxpMOPData::y2_init, sizeof(BxpMOPData::y2_init));
+  void save_state_in_file(string filename);
 
-     int len = cells.size();
-     file_obj.write((char*) &len, sizeof(int));
-     for (auto c:cells){
-       file_obj.write((char*) &c->box[0], sizeof(c->box[0])*c->box.size());
-       file_obj.write((char*) &cdata->a, sizeof(cdata->a));
-       file_obj.write((char*) &cdata->w_lb, sizeof(cdata->w_lb));
-       file_obj.write((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
-     }
+  void load_state_from_file(string filename, const IntervalVector& init_box);
 
-     len = paused_cells.size();
-     file_obj.write((char*) &len, sizeof(int));
-     for (auto c:paused_cells){
-       file_obj.write((char*) &c->box[0], sizeof(c->box[0])*c->box.size());
-       file_obj.write((char*) &cdata->a, sizeof(cdata->a));
-       file_obj.write((char*) &cdata->w_lb, sizeof(cdata->w_lb));
-       file_obj.write((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
-     }
-
-     len = ndsH.size();
-
-     file_obj.write((char*) &len, sizeof(int));
-     for (auto elem:ndsH.NDS2){
-       file_obj.write((char*) &elem.first[0], sizeof(elem.first[0])*elem.first.size());
-       file_obj.write((char*) &elem.second.n, sizeof(elem.second.n));
-       if(elem.second.n >= 1) file_obj.write((char*) &elem.second.x1[0], sizeof(elem.second.x1[0])*elem.second.x1.size());
-       if(elem.second.n == 2) file_obj.write((char*) &elem.second.x2[0], sizeof(elem.second.x2[0])*elem.second.x2.size());
-     }
-
-     for (auto p : ndsH.NDS2) cout << p.first << endl;
-
-     file_obj.close();
-  }
-
-  void load_state_from_file(string filename, const IntervalVector& init_box){
-    // Object to read from file
-    ifstream file_obj;
-
-    // Opening file in input mode
-    file_obj.open(filename, ios::in);
-    file_obj.read((char*)&BxpMOPData::y1_init, sizeof(BxpMOPData::y1_init));
-    file_obj.read((char*)&BxpMOPData::y2_init, sizeof(BxpMOPData::y2_init));
-
-    int len;
-    file_obj.read((char*) &len, sizeof(int));
-    for (int i=0; i<len; i++){
-      Cell* c=new Cell(init_box);
-      file_obj.read((char*)&c->box[0], sizeof(c->box[0])*c->box.size());
-      c->prop.add(new BxpMOPData());
-    	buffer.add_property(c->box, c->prop);
-    	bsc.add_property(c->box, c->prop);
-    	ctc.add_property(c->box, c->prop);
-
-      file_obj.read((char*) &cdata->a, sizeof(cdata->a));
-      file_obj.read((char*) &cdata->w_lb, sizeof(cdata->w_lb));
-      file_obj.read((char*) &cdata->ub_distance, sizeof(cdata->ub_distance));
-      cells.insert(c);
-    }
-
-    file_obj.read((char*) &len, sizeof(int));
-    for (int i=0; i<len; i++){
-      Cell* c=new Cell(init_box);
-      file_obj.read((char*)&c->box[0], sizeof(c->box[0])*c->box.size());
-      c->prop.add(new BxpMOPData());
-      buffer.add_property(c->box, c->prop);
-    	bsc.add_property(c->box, c->prop);
-    	ctc.add_property(c->box, c->prop);
-
-      file_obj.read((char*) c->prop[BxpMOPData::id], sizeof(BxpMOPData));
-      cells.insert(c);
-    }
-
-    file_obj.read((char*) &len, sizeof(int));
-    ndsH.NDS2.clear ();
-
-    for (int i=0; i<len; i++){
-      Vector y(2);
-      int nn;
-
-      file_obj.read((char*) &y[0], sizeof(y[0])*2);
-      file_obj.read((char*) &nn, sizeof(int));
-      Vector x1(1);
-      Vector x2(1); //or n+2?
-
-      if(nn>=1) {x1.resize(n); file_obj.read((char*) &x1[0], sizeof(x1[0])*(n));}
-      if(nn==2) {x2.resize(n); file_obj.read((char*) &x2[0], sizeof(x2[0])*(n));}
-
-      ndsH.NDS2[y]=NDS_data(x1,x2);
-      ndsH.NDS2[y].n=nn;
-    }
-    cout.precision(17);
-    for (auto p : ndsH.NDS2) cout << p.first << endl;
-
-
-    file_obj.close();
-  }
+	IStatus run(int maxiter=5, double eps=1e-1);
 
   void update_refpoint(Vector& refpoint);
 
  	void write_envelope(string output_file);
 
+  /****************************************/
 
   IntervalVector initbox;
 
